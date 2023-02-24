eeac7cf7301cb8f6f2b081a69db8751bc0b5bbe4
Philip Kristian Møller Flyvholm <phif@itu.dk>
2023-02-24 10:17:33 +0100

    Rebase

--- Language/src/parser/lexer/lexer.ts
+++ Language/src/parser/lexer/lexer.ts
@@ -1,7 +1,30 @@
-import gelex from "gelex";
+/*import gelex from "gelex";
 import { Lexer } from "./Interfaces";
 
 const def = gelex.definition();
+
+/*def.define("attribute", "[a-zA-Z-]*=", function (value: string) {
+  return value.substring(0, value.length - 1);
+});*/
+/*
+const idRegex = "[a-zA-Z_][a-zA-Z0-9_]*";
+def.define(LexTypes.topLevel, idRegex + ":", function (value: string) {
+  return value.substring(0, value.length - 1);
+});
+def.define(LexTypes.id, idRegex);
+def.define(LexTypes.number, "[0-9][0-9]*");
+def.defineText(LexTypes.string, '"', '"');
+def.define(LexTypes.newline, "\n");
+def.define(LexTypes.delimiter, ["{", "}", "(", ")", ";", ",", "/"]);
+
+import * as chevrotain from "@chevrotain/types";
+import { createToken } from "chevrotain";
+
+/*
+export function getLexer(data: any): Lexer {
+  return def.lexer(data);
+}
+*/
 export enum LexTypes {
   id = "id",
   topLevel = "toplevel",
@@ -11,23 +34,42 @@ export enum LexTypes {
   delimiter = "delimiter",
   latex = "latex",
 }
-/*def.define("attribute", "[a-zA-Z-]*=", function (value: string) {
-  return value.substring(0, value.length - 1);
-});*/
-const idRegex = "[a-zA-Z_][a-zA-Z0-9_]*";
-def.define(LexTypes.topLevel, idRegex + ":", function (value: string) {
-  return value.substring(0, value.length - 1);
-});
-def.define(LexTypes.id, idRegex);
-def.define(LexTypes.number, "[0-9][0-9]*");
-def.defineText(LexTypes.string, '"', '"');
-def.define(LexTypes.newline, "\n");
-def.define(LexTypes.delimiter, ["{", "}", "(", ")", ";", ",", "/", "$", "="]);
-def.define(LexTypes.latex, "[^$]*");
+const id = createToken({ name: LexTypes.id, pattern: /[a-zA-Z_][a-zA-Z0-9_]*/ })
 
-def.defineComment("//");
-def.defineComment("/*", "*/");
+const topLevel = createToken({
+  name: LexTypes.topLevel,
+  pattern: /[a-zA-Z_][a-zA-Z0-9_]*:/,
+  longer_alt: id, //Means that it will first also check id
+})
 
-export function getLexer(data: any): Lexer {
-  return def.lexer(data);
-}
+const string = createToken({
+  name: LexTypes.string,
+  pattern: /"[^"]*"/,
+})
+
+const newLine = createToken({
+  name: LexTypes.newline,
+  pattern: /\n/,
+  line_breaks: true,
+})
+
+const whiteSpace = createToken({
+  name: "WhiteSpace",
+  pattern: /\s+/,
+  group: chevrotain.Lexer.SKIPPED
+})
+
+const number = createToken({
+  name: LexTypes.number,
+  pattern: /[0-9][0-9]*/,
+})
+
+const delimiter = createToken({
+  name: LexTypes.delimiter,
+  pattern: /[{()};,\/$=]/,
+})
+
+const latex = createToken({
+  name: LexTypes.latex,
+  pattern: /\$[^$]*\$/,
+})
