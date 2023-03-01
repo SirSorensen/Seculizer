c2dbb8579fe691d13db50c1b9c6f8ccd4f4fc235
dasoITU <daso@itu.dk>
2023-03-01 15:39:31 +0100

    Particpant and participants components implemented

---
+++ web/src/lib/participant.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	export let name: String;
+    export let emoji: String;
+    export let knowledge: {Id:String, Emoji:String} [] = [];
+</script>
+
+<div class="container">
+    <div class="elem">
+        <p class="emoji">{emoji}</p>
+        <p>{name}</p>
+    </div>
+    {#each knowledge as {Id,Emoji}}
+        <div class="elem">
+            <p class="emoji">{Emoji}</p>
+            <p>{Id}</p>
+        </div>
+    {/each}
+</div>
+
+<style>
+    div.container {
+        background-color: #fff3d3;
+        align-items: center;
+        font-size: 1.5rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        border-radius: 1000px;
+        text-align: center;
+        box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
+    }
+
+    .emoji {
+        font-size: 4rem;
+    }
+
+    p {
+        margin: 0;    
+    }
+
+    .elem {
+        padding: 1rem;
+    }
+</style>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,3 +1,64 @@
+<script>
+    import Participants from "$lib/participants.svelte";
+    import Participant from "../lib/participant.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
 <p>Hello! I am alien from Florida!</p>
+
+<Participants
+    participants={[
+        {
+            Name: "Alice",
+            Emoji: "👩",
+            Knowledge: [
+                { Id: "a_sk", Emoji: "🔑" },
+                { Id: "your mom phone number", Emoji: "🤰🤙" },
+            ],
+        },
+        {
+            Name: "Bob",
+            Emoji: "👨",
+            Knowledge: [
+                { Id: "b_sk", Emoji: "🔑" },
+                { Id: "your dad phone number", Emoji: "🫃🤙" },
+            ],
+        },
+    ]}
+/>
+
+<Participant
+    name={"Alice"}
+    emoji={"👩"}
+    knowledge={[
+        { Id: "a_sk", Emoji: "🔑" },
+        { Id: "your mom phone number", Emoji: "🤰🤙" },
+    ]}
+/>
+<Participant
+    name={"Alice"}
+    emoji={"👩"}
+    knowledge={[
+        { Id: "a_sk", Emoji: "🔑" },
+        { Id: "your mom phone number", Emoji: "🤰🤙" },
+    ]}
+/>
+<Participant
+    name={"Alice"}
+    emoji={"👩"}
+    knowledge={[
+        { Id: "a_sk", Emoji: "🔑" },
+        { Id: "your mom phone number", Emoji: "🤰🤙" },
+    ]}
+/>
+<Participant
+    name={"Alice"}
+    emoji={"👩"}
+    knowledge={[
+        { Id: "a_sk", Emoji: "🔑" },
+        { Id: "your mom phone number", Emoji: "🤰🤙" },
+    ]}
+/>
