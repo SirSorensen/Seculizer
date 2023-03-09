d49d072a06915a6051883905769d2b8ceade2ffb
Philip Kristian Møller Flyvholm <phif@itu.dk>
2023-03-09 14:35:45 +0100

    A bit better design

---
+++ web/src/lib/Header.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Logo from "$lib/assets/logo.png"
+</script>
+<header>
+    <img src={Logo} alt="">
+</header>
+<style>
+    header {
+    width: 100vw;
+    height: 75px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #eeeeee;
+}
+
+img {
+    height: 90%;
+}
+</style>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import Editor from "$lib/Editor.svelte";
+  import Header from "$lib/Header.svelte";
   let content = "";
 
   let parsed: any = undefined;
-  
+
   function parseContent() {
     fetch("/api/generate", {
       method: "POST",
@@ -19,9 +20,49 @@
   }
 </script>
 
-{#if parsed}
-  <pre>{JSON.stringify(parsed, null, 2)}</pre>
-{:else}
-  <Editor bind:content />
-  <button disabled={content.trim() === ""} on:click={parseContent}>Generate</button>
-{/if}
+<div id="main">
+  <Header />
+  <div id="content">
+    {#if parsed}
+      <pre>{JSON.stringify(parsed, null, 2)}</pre>
+    {:else}
+      <Editor bind:content />
+      <button disabled={content.trim() === ""} on:click={parseContent}>Generate</button>
+    {/if}
+  </div>
+</div>
+
+<style>
+  #main{
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    min-height: 100vh;
+  }
+  
+  #content{
+    flex:1;
+    padding: 1rem;
+  }
+
+  button{
+    padding: 0.5rem;
+    border: none;
+    background: #FFF3D3;
+    border-radius: 0.5rem;
+    cursor: pointer;
+    margin: 1rem auto;
+    padding: 1rem 1.5rem;
+    display: block;
+    font-weight: bold;
+  }
+
+  button:disabled{
+    background: #f6f6f6;
+  }
+
+  button:hover:not(:disabled){
+    background: #f3e7c7;
+  }
+
+</style>
