0a3627eada0b831c06f013adbc339538c3bb1b65
Philip Kristian Møller Flyvholm <phif@itu.dk>
2023-03-11 12:21:10 +0100

    First Version

--- web/src/lib/components/messages/ActionBox.svelte
+++ web/src/lib/components/messages/ActionBox.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import Item from "./Item.svelte";
-
   export let title: string;
 </script>
 
--- web/src/lib/components/messages/Statement.svelte
+++ web/src/lib/components/messages/Statement.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
+  import ActionBox from "./ActionBox.svelte";
+  import Item from "../Item.svelte";
   import type { Statement as StatementAST } from "$lang/types/parser/interfaces";
+  import type { Program } from "$lib/program";
   export let statement: StatementAST;
+  export let program: Program;
 
 </script>
-
+{#if !statement.child}
+  <p>Invalid statement</p>
+{:else if statement.child.type === "clearStatement"}
+  {@const id = statement.child.id.value}
+  {@const icon = program.icons[id]}
+  <ActionBox title="clear"><Item emoji={icon} id={id}></Item></ActionBox>
+{:else if statement.child.type === "sendStatement"}
+  <p>Send</p>
+{/if}
