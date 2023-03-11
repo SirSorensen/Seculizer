a7b603a374729265a4a092f29a4b7d744337e5a7
Philip Kristian Møller Flyvholm <phif@itu.dk>
2023-03-11 17:57:40 +0100

    Implemented matchstatement, participantstatement

--- web/src/lib/components/messages/Statement.svelte
+++ web/src/lib/components/messages/Statement.svelte
@@ -4,6 +4,7 @@
   import Item from "../Item.svelte";
   import type { Statement as StatementAST } from "$lang/types/parser/interfaces";
   import type { Program } from "$lib/program";
+  import { getStringFromType } from "$lib/utils/stringUtil";
   export let statement: StatementAST;
   export let program: Program;
 </script>
@@ -13,7 +14,7 @@
 {:else if statement.child.type === "clearStatement"}
   {@const id = statement.child.id.value}
   {@const icon = program.icons[id]}
-  <ActionBox title="clear"><Item emoji={icon} {id} /></ActionBox>
+  <ActionBox title="clear"><Item emoji={icon} {id} /></ActionBox>"}
 {:else if statement.child.type === "sendStatement"}
   {@const leftId = statement.child.leftId.value}
   {@const rightId = statement.child.rightId.value}
@@ -33,5 +34,24 @@
         {alias}
       />
     {/each}
+  {:else if child.type === "matchStatement"}
+    {@const cases = child.cases}
+    {#each cases as matchCase}
+      <button>{getStringFromType(matchCase.case)}</button>
+    {/each}
+  {/if}
+{:else if statement.child.type === "participantStatement"}
+  {@const participantStatement = statement.child}
+  {#if !participantStatement.child}
+    <p>Invalid participant statement</p>
+  {:else if participantStatement.child.type === "newStatement"}
+    {@const id = participantStatement.child.id.value}
+    {@const icon = program.icons[id]}
+    <ActionBox title="new"><Item emoji={icon} {id} /></ActionBox>
+  {:else if participantStatement.child.type === "setStatement"}
+      {@const id = participantStatement.child.id.value}
+      {@const icon = program.icons[id]}
+      {@const value = participantStatement.child.value}
+      <ActionBox title="new"><div><Item emoji={icon} {id} /> = {getStringFromType(value)}</div></ActionBox>
   {/if}
 {/if}
