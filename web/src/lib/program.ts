d37ed960e440cbd7906b9d52d0889779d43d6185
daso <daso@itu.dk>
2023-03-14 14:10:24 +0100

    Implement Interface types in web/Program.ts + pipeStmnt remade + implemented different statementmethods

--- web/src/lib/program.ts
+++ web/src/lib/program.ts
@@ -1,35 +1,52 @@
 //import "../../../Language/dist/dist/dts/parser/interfaces"
-import type { Statement } from '$lang/types/parser/interfaces';
-
-type frame = {
-    next : frame | {[id: string]: frame}
-    prev : frame
-    participants :  {[id: string]: _participant}
-    presentation : Statement
-} | null
+import type { Participant, Statement, ParticipantStatement, 
+    SendStatement,
+    SetStatement,
+    MessageSendStatement,
+    MatchStatement,
+    KnowledgeItem,
+    Id,
+    Type,
+    FunctionDefItem,
+    FormatItem} from '$lang/types/parser/interfaces';
 
 
 type _participant = {
     name: string
-    knowledge: string[]
+    knowledge: _knowledge[]
 }
+
+type _knowledge = {
+    id: string
+    value: string
+}
+
+type participantMap = {[id: string]: _participant}
+
+type frame = {
+    next : frame | {[id: string]: frame}
+    prev : frame
+    participants :  participantMap
+    presentation : Statement
+} | null
 
 type _format = {
     //Function
     id: string
     params: string[]
+
     //Latex
     latex: string
 }
 
 export class Program {
-    init_participants: {[id: string]: _participant} = {}
+    init_participants: participantMap = {}
 
     first : frame = null
     last : frame = null
 
     keyRelations: {[id: string]: string} = {}
-    functions: {[id: string]: number} = {}
+    functions: {[id: string]: Number} = {}
     formats: {[id: string]: _format} = {}
     equations: {[id: string]: string} = {}
     icons: {[id: string]: string} = {}
@@ -45,9 +62,9 @@ export class Program {
         // Participants:
         // Create participants
         if (json.participants){
-            json.participants.participants.forEach((participant: any) => {
-                this.init_participants[participant.id] = {
-                    name: participant.id,
+            json.participants.participants.forEach((participant: Participant) => {
+                this.init_participants[participant.id.value] = {
+                    name: participant.id.value,
                     knowledge: []
                 }
             })
@@ -64,9 +81,15 @@ export class Program {
         // Knowledge:
         // Add knowledge to participants
         if (json.knowledge){
-            json.knowledge.knowledge.forEach((knowledge: any) => {
-                knowledge.children.forEach((child: any) => {
-                    this.init_participants[knowledge.id].knowledge.push(child.id)
+            json.knowledge.knowledge.forEach((knowledge: KnowledgeItem) => {
+                knowledge.children.forEach((child: Type) => {
+                    if (child.type == "function") throw new Error("Invalid json: stmnt child value type not implemented");
+                    else {
+                        this.init_participants[knowledge.id.value].knowledge.push({
+                            id: String(child.value),
+                            value: ""
+                        })
+                    }
                 })
             })
             if (log) console.log("Knowledge added to participants", this.init_participants);
@@ -84,8 +107,8 @@ export class Program {
 
         // Functions:
         if (json.functions){
-            json.functions.functions.forEach((func: any) =>
-                this.functions[func.id] = func.params
+            json.functions.functions.forEach((func: FunctionDefItem) =>
+                this.functions[func.id.value] = func.params
             )
             if (log) console.log("Functions created", this.functions);
         } else if (log) console.log("No functions found");
@@ -106,17 +129,23 @@ export class Program {
         // Format:
         // Add format to functions
         if (json.format){
-            json.format.formats.forEach((format: any) => {
+            json.format.formats.forEach((format: FormatItem) => {
+
                 let tmp_format : _format = {
                     id: format.function.id,
                     params: [],
-                    latex: format.function.latex
+                    latex: format.format.value
                 }
-                format.function.params.forEach((param: any) => {
-                    tmp_format.params.push(param.id)
+
+                format.function.params.forEach((param: Type) => {
+                    if (param.type == "function") throw new Error("Invalid json: stmnt child value type not implemented");
+                    else tmp_format.params.push(String(param.value));
                 })
+
                 this.formats[format.function.id] = tmp_format
+
             })
+
             if (log) console.log("Formats created", this.formats);
         } else if (log) console.log("No formats found");
 
@@ -133,10 +162,9 @@ export class Program {
         // Protocol:
 
         if (json.protocol.statements){
-            json.protocol.statements.forEach( (stmnt:any) => {
-                let type = this.getStmntType(stmnt)
+            json.protocol.statements.forEach( (stmnt: Statement) => {
                 let tmp_participants = this.last?.participants
-                tmp_participants = this.pipeStmnt(stmnt, type, tmp_participants)
+                tmp_participants = this.pipeStmnt(stmnt, tmp_participants)
                 this.newFrame(stmnt, tmp_participants)
             });
         } else if (log) console.log("No protocol found");
@@ -144,7 +172,7 @@ export class Program {
         console.log("Program created");
     }
 
-    newFrame(stmnt : any, participants: {[id: string]: _participant}){
+    newFrame(stmnt : any, participants: participantMap){
         let oldLast = this.last;
         this.last = {
             next: null,
@@ -156,52 +184,81 @@ export class Program {
         else this.first = this.last
     }
 
-    getStmntType(stmnt: any) : string {
-        if (stmnt.child){
-            let type = stmnt.child.type
-            if (type == "clearStatement") {
-                return type
-            } else if (type == "participantStatement" || type == "sendStatement") {
-                type = type.child.type
-                if (type) return type
-                else throw new Error("Invalid json: participantStatement child type not found");
-            } else {
-                return type
-            }
-        }
-        throw new Error("Invalid json: stmnt type not found");
-    }
-
-    pipeStmnt(stmnt:any, stmntType:string, participants: {[id: string]: _participant} | undefined){
+    pipeStmnt(stmnt: Statement, participants: participantMap | undefined) : participantMap{
         if (!participants) throw new Error("Invalid json: participants is undefined");
 
-        switch (stmntType) {
+        switch (stmnt.child.type) {
             case "clearStatement":
-                return this.clear(stmnt.child, participants)
-            case "newStatement":
+                return this.clearStmnt(stmnt.child.id.value, participants)
+            case "participantStatement":
+                return this.participantStmnt(stmnt.child, participants)
                 // TODO: create methond for newStatement
-            case "setStatement":
-                // TODO: create methond for newStatement
-            case "messageSendStatement":
-                // TODO: create methond for newStatement
-            case "matchStatement":
+            case "sendStatement":
+                return this.sendStmnt(stmnt.child, participants)
                 // TODO: create methond for newStatement
             default:
                 throw new Error("Invalid json: stmnt type not found");    
         }
     }
 
-    clear(knowledge: string, participants: {[id: string]: _participant}) : {[id: string]: _participant}{
+    clearStmnt(knowledge: string, participants: participantMap) : participantMap{
         Object.keys(participants).forEach((participant: string) => {
             participants[participant].knowledge =  participants[participant].knowledge.filter(
-                                                            (item: string) => item != knowledge
+                                                            (item: _knowledge) => item.id != knowledge
                                                         )
         })
         return participants;
     }
 
-    new(){
-
+    participantStmnt(stmnt : ParticipantStatement, participants: participantMap) : participantMap{        
+        // Pipe ParticipantStatement
+        if (stmnt.child.type == "newStatement"){
+            return this.newStmnt(stmnt.id.value, stmnt.child.id.value, participants)
+        } else if (stmnt.child.type == "setStatement"){
+            if (stmnt.child.value.type == "function") throw new Error("Invalid json: stmnt child value type not implemented");
+            else return this.setStmnt(stmnt.id.value, stmnt.child.id.value, String(stmnt.child.value.value), participants)
+        } else {
+            throw new Error("Invalid json: stmnt child type not implemented");
+        }
     }
+
+    newStmnt(participant : string, newKnowledge : string, participants: participantMap) : participantMap{
+        participants[participant].knowledge.push({
+            id: newKnowledge,
+            value: ""
+        })
+        return participants
+    }
+
+    setStmnt(participant : string, knowledge : string, value : string, participants: participantMap) : participantMap{
+        let index = participants[participant].knowledge.findIndex((element) => element.id == knowledge)
+
+        participants[participant].knowledge[index] = {
+            id: knowledge,
+            value: value
+        }
+
+        return participants
+    }
+
+    sendStmnt(stmnt : SendStatement, participants: participantMap) : participantMap{
+        // Pipe SendStatement
+        if (stmnt.child.type == "messageSendStatement"){
+            return this.messageSendStmnt(stmnt.child, participants)
+        } else if (stmnt.child.type == "matchStatement"){
+            return this.matchStmnt(stmnt.child, participants)
+        } else {
+            throw new Error("Invalid json: stmnt child type not implemented");
+        }
+    }
+
+    messageSendStmnt(stmnt : MessageSendStatement, participants: participantMap) : participantMap{
+        return participants
+    }
+
+    matchStmnt(stmnt : MatchStatement, participants: participantMap) : participantMap{
+        return participants
+    }
+    
 }
 
