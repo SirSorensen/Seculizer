7780a4988f6c670c2a2598d18c3c8a6e867d457b
daso <daso@itu.dk>
2023-04-18 11:02:18 +0200

    implement cloneFunctionChangedParam

--- web/src/lib/models/EquationMap.ts
+++ web/src/lib/models/EquationMap.ts
@@ -12,7 +12,7 @@ type equalResult = {
 type queueElement = {
   f: FunctionCall;
   search_depth: number;
-  param_depth: number;
+  param_depth: number[];
 };
 
 export class EquationMap {
@@ -27,35 +27,42 @@ export class EquationMap {
   }
 
   doesParticipantKnow(parti: Participant, f: FunctionCall, val: Type | undefined = undefined): boolean {
-    let history: Map<string, boolean> = new Map();
-    let queue: queueElement[] = [];
+    const history: Map<string, boolean> = new Map();
+    const queue: queueElement[] = [];
     if (this.equations[f.id] === undefined) return Equal.checkIfInputisKnown(f, parti);
-    let maxDepth = this.calcMaxDepth(f);
+    const maxDepth = this.calcMaxDepth(f);
     
-    queue.push({ f: f, search_depth: 0, param_depth: 0});
+    queue.push({ f: f, search_depth: 0, param_depth: []});
 
     //TODO: generateEquals for inner functions
     while (queue.length > 0) {
-      let current = queue.shift();
+      const current = queue.shift();
       if (current === undefined) continue;
       if (current.search_depth > maxDepth) continue;
 
-      let _f = current.f
+      const _f = current.f
       if (history.has(getStringFromType(_f))) continue;
       history.set(getStringFromType(_f), true);
 
       if (Equal.checkIfInputisKnown(_f, parti)) return true;
 
       for (const eq of this.equations[_f.id].eqs) {
-        let _fEq = eq.generateEqual(_f);
+        const _fEq = eq.generateEqual(_f);
         queue.push({ f: _fEq, search_depth: current.search_depth + 1, param_depth: current.param_depth });
       }
 
-      let param_depth = this.calcParamDepth(_f, current.param_depth);
-      if (param_depth !== undefined) {
-        maxDepth += this.equations[(f.params[param_depth - 1] as FunctionCall).id].maxDepth;
-        queue.push({ f: _f, search_depth: current.search_depth + 1, param_depth: current.param_depth + 1 });
-      };
+      const tmpParam_depth = this.calcParamDepth(_f, current.param_depth);
+      while (tmpParam_depth.length > 0) {
+        for (const eq of this.equations[_f.id].eqs) {
+          const _fParamEq = eq.generateEqual(this.getParamFunctionFromDepth(_f, current.param_depth));
+
+          queue.push({
+            f: this.cloneFunctionChangedParam(_f, current.param_depth, _fParamEq),
+            search_depth: current.search_depth + 1,
+            param_depth: current.param_depth.concat([tmpParam_depth.shift() as number]),
+          });
+        }
+      }
     }
 
     return false;
@@ -73,22 +80,54 @@ export class EquationMap {
     return maxDepth;
   }
 
-  private calcParamDepth(f: FunctionCall, param_depth : number, i : number = 0): number | undefined {
+  private calcParamDepth(f: FunctionCall, param_depth : number[]): number[] {
+    // Arrange variables
+    const _f: FunctionCall = this.getParamFunctionFromDepth(f, param_depth);
+    const index: number[] = [];
 
-    const aux = (_f : FunctionCall) => {
-        for (const param of f.params) {
-        i += 1;
-        if (param.type == "function"){ 
-          if (i > param_depth) return i;
-          else aux(param)
-        }
-      }
-
-      return undefined;
+    //Build index
+    for(let i = 0; i < _f.params.length; i++){
+      if(_f.params[i].type == "function") index.push(i);
     }
 
-    aux(f);
-    return i;
+    return index
+  }
+
+  private getParamFunctionFromDepth(f: FunctionCall, param_depth : number[]): FunctionCall {
+    let _f: FunctionCall = f;
+    const index = param_depth.map((x) => x);
+
+    while (index.length > 0) {
+      const i = index.shift();
+      if (i === undefined) throw new Error("param_depth is not defined correctly");
+      if (_f.params[i] === undefined || _f.params[i].type != "function") throw new Error("param_depth is not correct");
+      _f = _f.params[i] as FunctionCall;
+    }
+
+    return _f;
+  }
+
+  cloneFunctionChangedParam(f: FunctionCall, param_depth: number[], newParam: FunctionCall): FunctionCall {
+    const _f = structuredClone(f);
+    const _param_depth = param_depth.map((x) => x);
+
+    const aux = (aux_f: FunctionCall) => {
+      // If _param_depth is empty, we are at the function we want to change
+      if (_param_depth.length === 0) return newParam;
+
+      // Get the index of the next function
+      const i = _param_depth.shift();
+      
+      //Check if i and aux_f.params[i] are valid
+      if (i === undefined) throw new Error("param_depth is not defined correctly");
+      if (aux_f.params[i] === undefined || aux_f.params[i].type != "function") throw new Error("param_depth is not correct");
+
+      // Call aux with the next function and change the param[i]
+      aux_f.params[i] = aux(aux_f.params[i] as FunctionCall);
+      return aux_f
+    };
+
+    return aux(_f);
   }
 
   private functionIdParameter(f: FunctionCall): number {
