0d9940fdd10a35b262e2caf6a456cee3adf6b624
daso <daso@itu.dk>
2023-04-25 10:07:36 +0200

    fixed #63 LatexMap where functionCalls are saved as "name" + "!p" + "parameter.length"

--- web/src/lib/models/LatexMap.ts
+++ web/src/lib/models/LatexMap.ts
@@ -1,21 +1,25 @@
-import type { Type } from "$lang/types/parser/interfaces";
+import type { FunctionCall, Type } from "$lang/types/parser/interfaces";
 import { Latex } from "./Latex";
 
 export class LatexMap {
   latexMap: { [id: string]: Latex } = {};
 
+  private constructKey(call : FunctionCall){
+    return call.id + "!p" +  call.params.length;
+  }
+
   addLatex(call: Type, latex: string) {
-    if (call.type == "function") this.latexMap[call.id] = new Latex(call, latex);
+    if (call.type == "function") this.latexMap[this.constructKey(call)] = new Latex(call, latex);
     else this.latexMap[call.value] = new Latex(call, latex);
   }
   contains(call: Type) {
-    if (call.type == "function") return this.latexMap[call.id] != undefined;
+    if (call.type == "function") return this.latexMap[this.constructKey(call)] != undefined;
     else return this.latexMap[call.value] != undefined;
   }
 
   getConstructedLatex(call: Type): string {
     if (call.type == "function") {
-      if (this.latexMap[call.id] == undefined) {
+      if (this.latexMap[this.constructKey(call)] == undefined) {
         let str = "$" + call.id + "(";
         str += call.params.map((param) => {
           if (param.type == "function") return this.getConstructedLatex(param);
@@ -26,7 +30,7 @@ export class LatexMap {
         return str;
       }
 
-      return this.latexMap[call.id].constructLatex(call, this);
+      return this.latexMap[this.constructKey(call)].constructLatex(call, this);
     } else {
       if (this.latexMap[call.value] == undefined) return call.value as string;
 
